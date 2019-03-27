e6375800a8b8cbd1e322472f3d4109d6854272bb
uncleseneca <uncleseneca@gmail.com>
2019-03-27 17:03:24 +0300

    feat(config): add config module

--- src/back/app.module.ts
+++ src/back/app.module.ts
@@ -1,7 +1,12 @@
 import { Module, NestModule } from '@nestjs/common'
+import { ConfigModule } from './config/config.module'
+
 import FrontendController from './FrontendController'
 
-@Module({ controllers: [FrontendController] })
+@Module({
+  controllers: [FrontendController],
+  imports: [ConfigModule],
+})
 export class AppModule implements NestModule {
   public configure() {
     // pass
